5a1a0a9a86da7b40d5ee0aadbfa601df86f2023d
minjaposao <minjaposao@gmail.com>
2025-06-19 12:30:26 +0200

    six commit

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,22 +1,29 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import clientPromise from '@/lib/mongodb'; // ili gde već držiš konekciju
 import { ObjectId } from 'mongodb';
 
-export async function DELETE(request: Request, { params }: { params: { id: string } }) {
-  const client = await clientPromise;
-  const db = client.db();
+export async function DELETE(
+    request: NextRequest,
+    { params }: { params: Promise<{ id: string }> }
+) {
 
-  try {
-    const result = await db.collection('users').deleteOne({
-      _id: new ObjectId(params.id),
-    });
+    const {id} = await params;
 
-    if (result.deletedCount === 0) {
-      return NextResponse.json({ message: 'User not found' }, { status: 404 });
+    const client = await clientPromise;
+    const db = client.db();
+
+    try {
+        const result = await db.collection('users').deleteOne({
+            _id: new ObjectId(id),
+        });
+
+        if (result.deletedCount === 0) {
+            return NextResponse.json({ message: 'User not found' }, { status: 404 });
+        }
+
+        return NextResponse.json({ message: 'User deleted' });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: 'Something went wrong' }, { status: 500 });
     }
-
-    return NextResponse.json({ message: 'User deleted' });
-  } catch (error) {
-    return NextResponse.json({ error: 'Something went wrong' }, { status: 500 });
-  }
 }
